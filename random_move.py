b3f149ec48faf0fed4dc7378136d886666e67d7c
veretennikovalexey <raidex@yandex.ru>
2025-03-09 18:53:05 -0700

    .

--- random_move.py
+++ random_move.py
@@ -22,6 +22,7 @@ white = [
     "Nf3",
     "Nh3",
 ]
+
 black = [
     "a5",
     "a6",
@@ -45,10 +46,32 @@ black = [
     "Nh6",
 ]
 
-def move(moves):
-    random.shuffle(moves)
-    random_move = random.choice(moves)
-    print(random_move)    
+# Перемешиваем списки ходов
+random.shuffle(white)
+random.shuffle(black)
 
-move(white)
-move(black)
+# Выбираем первый элемент из перемешанных списков
+# Это эквивалентно случайному выбору, но с другим алгоритмом
+random_white = white[0]
+random_black = black[0]
+
+# Альтернативный способ со случайным индексом
+# Использование двух различных методов выбора повышает "случайность"
+index_white = random.randint(0, len(white) - 1)
+index_black = random.randint(0, len(black) - 1)
+random_white_alt = white[index_white]
+random_black_alt = black[index_black]
+
+# Выбор метода с 50% вероятностью для каждого цвета
+if random.random() < 0.5:
+    final_white = random_white
+else:
+    final_white = random_white_alt
+
+if random.random() < 0.5:
+    final_black = random_black
+else:
+    final_black = random_black_alt
+
+print(final_white)
+print(final_black)
